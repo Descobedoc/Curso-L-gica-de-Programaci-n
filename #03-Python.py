8b68608c15afcdd38b120609094e60f63d59ec58
Descobedoc <descobedoc@gmail.com>
2025-12-17 16:19:54 +0100

    Revert "#03-Python modificado"
    This reverts commit df50f0eb12831ce333244ea9d2c5622a53f14a81.

--- #03-Python.py
+++ #03-Python.py
@@ -2,24 +2,25 @@
 
 # Listas
 
-my_list = ["David", "Maribel", "Joel", "Oriol"]
+my_list = ["David", "Escobedo", "darkfenris", 52, 1.79]
+my_other_list = [20, 52, 30, 28, 45, 52, 18]
 
 print(len(my_list)) # len nos indica el número de elementos de la lista
 print(type(my_list)) # type nos indica la clase, en este caso clase list
 print(my_list[0]) # Accedemos al valor en la posición indicada
+print(my_other_list.count(52)) # count indica las veces que está el elemento indicado en la lista
 
-my_list.append("Kira") # append añade un elemento al final de la lista
+my_list.append("descobedoc@gmail.com") # append añade un elemento al final de la lista
 print(my_list)
 
-my_list.insert(3, "Oddie") # insert añade un elemento en la posición indicada
+my_list.insert(3, "Verde") # insert añade un elemento en la posición indicada
 print(my_list)
 
-my_list[3] = "Gia" # Se asigna un valor a una posición determinada, modificando el valor existente
+my_list[3] = "Azul" # Se asigna un valor a una posición determinada, modificando el valor existente
 print(my_list)
 
-my_list.remove("Gia") # remove borra el elemento indicado
+my_list.remove("Azul") # remove borra el elemento indicado
 print(my_list)
 
-my_list.sort() # sort ordena la lista de forma alfabética
-print(my_list)
-
+my_other_list.pop
+print(my_other_list)
