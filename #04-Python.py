9ca9e634014fb31a346a58f2014d622d299ddf5d
Descobedoc <descobedoc@gmail.com>
2025-12-19 13:38:44 +0100

    Principio caracteres

--- #04-Python.py
+++ #04-Python.py
@@ -1,4 +1,23 @@
-# Cadenas de caracteres
+# Operaciondes con cadenas de caracteres
 
-my_string = "En un lugar de la Mancha de cuyo nombre no quiero acordarme"
-print(my_string.count("d"))
+s1 = "David"
+s2 = "Escobedo"
+
+# Concatenación
+print(s1 + " " + s2)
+
+# Repetición
+print(s1 * 3)
+
+# Indexación
+print(s1[2])
+
+# Longitud
+print(len(s2))
+
+# Slicing
+print(s2[2:6])
+print(s2[2:])
+print(s2[:4])
+
+# Búsqueda
